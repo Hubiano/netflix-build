740ba7bfd1739bb67ba9b66d122c07a97e58a0d7
Ivan Florenz <triskelion22@hotmail.com>
2021-02-06 16:54:19 -0800

    created the front end of the netflix app, added the Home screen, and the Row and the nav components

---
+++ firebase.readme.md
@@ -0,0 +1,16 @@
+npm install -g firebase-tools
+
+firebase login
+
+firebase init
+
+firebase deploy
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIUPT_BsSmMrbxqRJKyNNhRBD2dv6lgho",
+  authDomain: "netflix-build-6f1e4.firebaseapp.com",
+  projectId: "netflix-build-6f1e4",
+  storageBucket: "netflix-build-6f1e4.appspot.com",
+  messagingSenderId: "334603393812",
+  appId: "1:334603393812:web:536248889f1470b63bc001"
+};
---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./nav.css";
+
+function Nav() {
+  const [show, setShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.freepnglogos.com%2Fuploads%2Fnetflix-logo-0.png&f=1&nofb=1"
+          alt="netflix"
+        />
+        <img
+          className="nav_avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
